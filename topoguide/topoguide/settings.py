9a1dc52ca5e96f88548b16ca0368d8134107bb52
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-18 01:34:34 +0200

    Add redirect path for login and logout

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'topoguide.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirection after login and logout
+
+LOGIN_REDIRECT_URL = True
+
+LOGOUT_REDIRECT_URL = True
