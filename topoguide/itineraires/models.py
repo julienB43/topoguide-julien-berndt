bd1599db9cd37f4d4cde0bbe24c1999567c8b915
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-18 20:06:37 +0200

    Update model to fit with subject

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,18 +1,18 @@
 from tkinter import CASCADE
 from django.db import models
 
-class Itineraires(models.Model):
-    title = models.CharField(max_length=200)
-    start = models.CharField(max_length=200)
+class Itineraire(models.Model):
+    
+    nom = models.CharField(max_length=200)
+    départ = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     start_alt = models.IntegerField('Altitude de départ (m)')
     alt_min = models.IntegerField('Altitude minimale (m)')
     alt_max = models.IntegerField('Altitude maximale (m)')
     deniv_pos_cumul = models.IntegerField('Dénivelé positif cumulé (m)')
     deniv_neg_cumul = models.IntegerField('Dénivelé négatif cumulé (m)')
-    
-    duration = models.IntegerField('Durée (min)')
-    dificulty = models.IntegerField('Dificulté (1-5)')
+    duration = models.IntegerField('Durée (h)')
+    difficulty = models.IntegerField('Difficulté (1-5)')
     
     def __self__(self):
-        return self.title
+        return self.nom
