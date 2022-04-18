6bf7a8e64dd28142370bb9a719f90e2e00cc4fc8
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-18 17:25:56 +0200

    Add the Itineraires model

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,3 +1,18 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+class Itineraires(models.Model):
+    title = models.CharField(max_length=200)
+    start = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    start_alt = models.IntegerField('Altitude de départ (m)')
+    alt_min = models.IntegerField('Altitude minimale (m)')
+    alt_max = models.IntegerField('Altitude maximale (m)')
+    deniv_pos_cumul = models.IntegerField('Dénivelé positif cumulé (m)')
+    deniv_neg_cumul = models.IntegerField('Dénivelé négatif cumulé (m)')
+    
+    duration = models.ForeignKey('Sortie', on_delete=CASCADE)
+    dificulty = models.ForeignKey('Sortie', on_delete=CASCADE)
+    
+    def __self__(self):
+        return self.title
