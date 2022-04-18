56d737d23ea96286570cf46056a14a7c3c60a18c
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-18 21:32:25 +0200

    Update itineraire view as basic view

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Itineraire
 
 def itineraires(request):
     
-    itineraire = get_list_or_404(Itineraire)
+    itineraire = get_object_or_404(Itineraire)
     
     return render(request, 'itineraires/itineraires.html', {'itineraire': itineraire})
