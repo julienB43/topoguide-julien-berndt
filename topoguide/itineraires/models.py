26c7552d57fd1315344eb2b8d876fd48c0db8ed3
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-18 23:36:33 +0200

    Add sortie views and make it adminitrated in admin

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,11 +1,11 @@
-from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 class Itineraire(models.Model):
     
-    nom = models.CharField(max_length=200)
-    départ = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
+    title = models.CharField('Nom', max_length=200)
+    start = models.CharField('Point de départ', max_length=200)
+    description = models.CharField('Description', max_length=1000)
     start_alt = models.IntegerField('Altitude de départ (m)')
     alt_min = models.IntegerField('Altitude minimale (m)')
     alt_max = models.IntegerField('Altitude maximale (m)')
@@ -16,3 +16,16 @@ class Itineraire(models.Model):
     
     def __self__(self):
         return self.nom
+    
+class Sortie(models.Model):
+    
+    User = models.ForeignKey('User', on_delete=models.CASCADE)
+    itineraire = models.ForeignKey('Itineraire', on_delete=models.CASCADE)
+    
+    date_sortie = models.DateTimeField('Date de la sortie')
+    real_duration = models.IntegerField('Durée réelle (h)')
+    nb_people = models.IntegerField('Nombre de participants')
+    exp_grp = models.CharField(choices=['tous débutants', 'tous expérimentés', 'mixte'])
+    weather = models.CharField(choices=['bonne', 'moyenne', 'mauvaise'])
+    difficulty_felt = models.IntegerField('Difficulté (1-5)')
+
