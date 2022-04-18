2d6d84afa12992db194f7a86a2368aff693df886
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-18 20:10:55 +0200

    Add views with dummy templates inherited

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Itineraire
+
+def itineraires(request):
+    
+    itineraire = get_list_or_404(Itineraire)
+    
+    return render(request, 'itineraires/itineraires.html', {'itineraire': itineraire})
