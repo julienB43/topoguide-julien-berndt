8d4949317c25df45b0ae69203c9b1fd4e397480c
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-19 11:03:40 +0200

    Add view to sortie page with its template

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_list_or_404
 
-from .models import Itineraire
+from .models import Itineraire, Sortie
 
 def itineraire(request):
     itineraires = get_list_or_404(Itineraire)
     return render(request, 'itineraires/itineraires.html', {'itineraires': itineraires})
+
+def sorties(request, itineraire_id):
+    sorties = get_list_or_404(Sortie, pk=itineraire_id)
+    return render(request, 'itineraires/sorties.html', {'itineraires': sorties})
