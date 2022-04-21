798baa5d77eb2c7b9f5eaecdf385191d6fdc65fc
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-21 08:52:14 +0200

    Update view for modification (last one was the creation)

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Itineraire, Sortie
 from .form import SortieForm
@@ -15,6 +16,7 @@ def sortie(request, user_id):
     sortie = get_object_or_404(Sortie, pk=user_id)
     return render(request, 'itineraires/sortie_detail.html', {'sortie': sortie})
 
+@login_required
 def nouvelle_sortie(request):  
     if request.method == 'GET':
         form = SortieForm()
@@ -24,3 +26,15 @@ def nouvelle_sortie(request):
             form.save()
             return redirect('itineraires')
     return render(request,'itineraires/nouvelle_sortie.html', {'form': form})
+
+@login_required
+def modif_sortie(request, user_id):
+    sortie = Sortie.objects.get(pk=user_id)
+    if request.method == 'GET':
+        form = SortieForm(instance=sortie)
+    elif request.method == 'POST':
+        form = SortieForm(request.POST, instance=sortie)
+        if form.is_valid():
+            form.save()
+            return redirect('itineraires')
+    return render(request, 'itineraires/modif_sortie.html', {'form': form})
