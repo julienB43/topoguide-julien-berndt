591a8a6bb1b65fd75e466eb7913177f4977cd38c
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-22 00:33:48 +0200

    Correction of the login redirection path

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -126,4 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redirection after login and logout
 
-LOGIN_REDIRECT_URL = '/itineraires/itineraire'
+LOGIN_REDIRECT_URL = '/itineraires'
