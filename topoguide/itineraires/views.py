861af562d4f57e970fbc5a1a494f28857b1f16bb
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-21 18:18:44 +0200

    Add logout redirect and itineraire detail

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 from .models import Itineraire, Sortie
 from .form import SortieForm
@@ -10,12 +11,18 @@ def itineraire(request):
 
 def sorties(request, itineraire_id):
     sorties = Sortie.objects.filter(itineraire_id=itineraire_id)
-    return render(request, 'itineraires/sorties.html', {'sorties': sorties})
+    itineraire_detail = get_object_or_404(Itineraire, pk=itineraire_id)
+    return render(request, 'itineraires/sorties.html', {'sorties': sorties, 'itineraire_detail': itineraire_detail})
 
 def sortie(request, user_id):
     sortie = get_object_or_404(Sortie, pk=user_id)
     return render(request, 'itineraires/sortie_detail.html', {'sortie': sortie})
 
+@login_required()
+def logout_redirect(request):
+    logout(request)
+
+@login_required()
 def nouvelle_sortie(request):
     if request.method == 'GET':
         form = SortieForm()
@@ -26,6 +33,7 @@ def nouvelle_sortie(request):
             return redirect('itineraires:itineraire')
     return render(request,'itineraires/nouvelle_sortie.html', {'form': form})
 
+@login_required()
 def modif_sortie(request, user_id):
     sortie = Sortie.objects.get(pk=user_id)
     if request.method == 'GET':
